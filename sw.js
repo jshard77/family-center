394aa8d581d73ea60332bc0738d930ba098cbfed
jshard77 <jshard77@gmail.com>
2026-09-15 19:33:06 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,3 @@
+const CACHE='family-center-test-v1';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));
+self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
