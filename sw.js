ef88756e3ac8b8ddfebe5c196fd8f9e80ed92810
jshard77 <jshard77@gmail.com>
2026-09-15 20:17:02 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "family-center-phase2-v1";
+const CACHE_NAME = "family-center-phase2-v2";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
